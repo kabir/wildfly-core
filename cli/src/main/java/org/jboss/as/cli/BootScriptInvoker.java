5ce0a07eecc0f5c2790428c67788eba2dd575fe4
JF Denise <jdenise@redhat.com>
2019-11-28 21:17:49 +0100

    Add support for properties file, needed in Openshift context

--- cli/src/main/java/org/jboss/as/cli/BootScriptInvoker.java
+++ cli/src/main/java/org/jboss/as/cli/BootScriptInvoker.java
@@ -17,9 +17,14 @@ package org.jboss.as.cli;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 import org.jboss.as.controller.AdditionalBootCliScriptInvoker;
 import org.jboss.as.protocol.StreamUtils;
+import org.wildfly.security.manager.WildFlySecurityManager;
 
 /**
  *
@@ -30,6 +35,15 @@ public class BootScriptInvoker implements AdditionalBootCliScriptInvoker {
     @Override
     public void runCliScript(File file) {
         CommandContext ctx;
+        String props = WildFlySecurityManager.getPropertyPrivileged("org.wildfly.cli.boot.script.properties", null);
+        if (props != null) {
+            File propsFile = new File(props);
+            if (!propsFile.exists()) {
+                // TODO i18n
+                throw new RuntimeException("Could not find file " + propsFile.getAbsolutePath());
+            }
+            handleProperties(propsFile);
+        }
         try {
             ctx = CommandContextFactory.getInstance().newCommandContext();
         } catch (CliInitializationException ex) {
@@ -52,6 +66,21 @@ public class BootScriptInvoker implements AdditionalBootCliScriptInvoker {
             throw new IllegalStateException("Failed to process file '" + file.getAbsolutePath() + "'", e);
         } finally {
             StreamUtils.safeClose(reader);
+        }
+    }
+
+    private static void handleProperties(File propertiesFile) {
+        final Properties props = new Properties();
+        try ( InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(propertiesFile), StandardCharsets.UTF_8)) {
+            props.load(inputStreamReader);
+            for (String key : props.stringPropertyNames()) {
+                WildFlySecurityManager.setPropertyPrivileged(key, props.getProperty(key));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        for (String key : props.stringPropertyNames()) {
+            WildFlySecurityManager.setPropertyPrivileged(key, props.getProperty(key));
         }
     }
 }
